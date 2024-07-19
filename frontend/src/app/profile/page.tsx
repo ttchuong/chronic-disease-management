4efee36d527731ad37210489a965de2622934f75
Toni Pham <tonidevvn@gmail.com>
2024-07-19 12:20:18 -0400

    update for overview page

--- frontend/src/app/profile/page.tsx
+++ frontend/src/app/profile/page.tsx
@@ -71,14 +71,14 @@ const Profile = () => {
             <div className="relative z-30 mx-auto -mt-22 h-30 w-full max-w-30 rounded-full bg-white/20 p-1 backdrop-blur sm:h-44 sm:max-w-44 sm:p-3">
               <div className="relative drop-shadow-2">
                 <Image
-                  src={"/images/user/user-06.png"}
-                  width={160}
-                  height={160}
-                  style={{
-                    width: "auto",
-                    height: "auto",
-                  }}
-                  alt="profile"
+                    width={160}
+                    height={160}
+                    src={"/images/user/user-06.png"}
+                    style={{
+                      width: "auto",
+                      height: "auto",
+                    }}
+                    alt="User"
                 />
                 <label
                   htmlFor="profile"
@@ -116,9 +116,9 @@ const Profile = () => {
             </div>
             <div className="mt-4">
               <h3 className="mb-1.5 text-2xl font-semibold text-black dark:text-white">
-                Danish Heilium
+                Stephen Conley
               </h3>
-              <p className="font-medium">Ui/Ux Designer</p>
+              <p className="font-medium">Cardiologist</p>
               <div className="mx-auto mb-5.5 mt-4.5 grid max-w-94 grid-cols-3 rounded-md border border-stroke py-2.5 shadow-1 dark:border-strokedark dark:bg-[#37404F]">
                 <div className="flex flex-col items-center justify-center gap-1 border-r border-stroke px-4 dark:border-strokedark xsm:flex-row">
                   <span className="font-semibold text-black dark:text-white">
@@ -142,20 +142,16 @@ const Profile = () => {
 
               <div className="mx-auto max-w-180">
                 <h4 className="font-semibold text-black dark:text-white">
-                  About Me
+                  About Dr. Stephen Conley (he/him)
                 </h4>
                 <p className="mt-4.5">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Pellentesque posuere fermentum urna, eu condimentum mauris
-                  tempus ut. Donec fermentum blandit aliquet. Etiam dictum
-                  dapibus ultricies. Sed vel aliquet libero. Nunc a augue
-                  fermentum, pharetra ligula sed, aliquam lacus.
+                  Dr. Stephen Conley is a distinguished cardiologist known for his expertise in diagnosing and treating heart conditions. With extensive experience in both clinical practice and research, Dr. Conley has dedicated his career to advancing cardiovascular health and patient care. He holds a medical degree from a prestigious institution and has completed rigorous training in cardiology. Dr. Conley is highly regarded for his compassionate approach to patient care and his commitment to staying at the forefront of cardiological advancements. He actively participates in medical conferences and contributes to scholarly publications, continually enhancing his knowledge and skills to provide the best possible care for his patients.
                 </p>
               </div>
 
               <div className="mt-6.5">
                 <h4 className="mb-3.5 font-medium text-black dark:text-white">
-                  Follow me on
+                  Follow on
                 </h4>
                 <div className="flex items-center justify-center gap-3.5">
                   <Link
