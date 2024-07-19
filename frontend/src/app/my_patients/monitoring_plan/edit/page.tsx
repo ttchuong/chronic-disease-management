3eb19218e6d75b08d18af558cefa3a017606edd7
chuong <chuongtat@gmail.com>
2024-07-18 23:30:28 -0400

    Fix ESLint

--- frontend/src/app/my_patients/monitoring_plan/edit/page.tsx
+++ frontend/src/app/my_patients/monitoring_plan/edit/page.tsx
@@ -127,7 +127,7 @@ const MonitoringPlanEdit = () => {
       nextArrow:
         '<svg className="fill-current" width="7" height="11" viewBox="0 0 7 11"><path d="M1.4 10.8L0 9.4l4-4-4-4L1.4 0l5.4 5.4z" /></svg>',
     });
-  }, []);
+  }, [dateOfConsultation]);
 
   return (
     <div className="flex flex-col flex-1 gap-8 text-sm">
